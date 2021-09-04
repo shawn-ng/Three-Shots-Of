a02f777db90f588135e7455c1db1db1b2dfaa6df
Lillian Monza <lillianmonza@Lillians-MacBook-Pro.local>
2021-09-04 12:36:21 +0100

    Mood + Sad - Minor element & copy changes

--- Three-Shots-Of/tso-react-app/src/components/moods/Sad.js
+++ Three-Shots-Of/tso-react-app/src/components/moods/Sad.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Sad = () => {
   return (
@@ -6,13 +6,13 @@ const Sad = () => {
       <div className="hero-body">
         <div className="container is-flex is-justify-content-center">
           <img
-            src="https://media.giphy.com/media/8tVRZucyVinIY/giphy.gif"
+            src="https://media.giphy.com/media/l0HlyqWEL9Q4Ynx60/giphy.gif"
             alt="stewie need love"
           />
         </div>
         <p className="title has-text-centered mt-6">
           <a className="link" href="https://www.netflix.com/gb/Login">
-            Click Me !
+            We have something special for you here 😉!
           </a>
         </p>
       </div>
